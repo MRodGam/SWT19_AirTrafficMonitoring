fc8ce2a9c6c5f4c050f51c714921f024fe821488
Matilde Gammelgaard <matildegammelgaard@hotmail.com>
2019-03-07 12:00:05 +0100

    Setup metode til test af Formatter-FormatData metode

---
+++ ATM/ATM_UnitTest/Formatter/FormatData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+using NSubstitute;
+using ATM;
+using TransponderReceiver;
+
+namespace ATM_UnitTest
+{
+    [TestFixture]
+    public class FormatData
+    {
+        private ITransponderReceiver _fakeTransponderReceiver;
+        private IFormatter _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Make a fake Transponder Data Receiver
+            _fakeTransponderReceiver = Substitute.For<ITransponderReceiver>();
+            // Inject the fake TDR
+            _uut = new Formatter(_fakeTransponderReceiver);
+        }
+
+
+    }
+}
