33d2a5d6c4c06a91b3275eaba4e6416e43709eeb
Matilde Gammelgaard <matildegammelgaard@hotmail.com>
2019-03-12 11:35:52 +0100

    Tilføjelse af metode i Air Traffic controller. Ingen unittest

--- ATM/ATM/AirTrafficController.cs
+++ ATM/ATM/AirTrafficController.cs
@@ -10,22 +10,50 @@ namespace ATM
     public class AirTrafficController
     {
         private IFormatter receiver;
-        public FormattedData CurrentData { get; set; }
+        public FormattedData CurrentData { get; private set; }
+        public FormattedData OldData { get; private set; }
+        public List<FormattedData> AircraftsInAirspace;
 
-        public AirTrafficController(IFormatter receiver)
+
+        public AirTrafficController(IFormatter receiver,)
         {
             // This will store the real or the fake transponder data receiver
             this.receiver = receiver;
 
             // Attach to the event of the real or the fake TDR
             this.receiver.FormattedDataReady += ReceiverOnFormattedDataReady;
+
+            AircraftsInAirspace = new List<FormattedData>();
         }
 
         private void ReceiverOnFormattedDataReady(object sender, FormattedDataEventArgs e)
         {
             CurrentData = e.FormattedData;
-            System.Console.WriteLine("Transponderdata Tag: {0} Placement: {1},{2} Altitude: {3}, Timestamp: {4}", CurrentData.Tag, CurrentData.XCoordinate,
-                CurrentData.YCoordinate, CurrentData.Altitude, CurrentData.TimeStamp);
+            //System.Console.WriteLine("Transponderdata Tag: {0} Placement: {1},{2} Altitude: {3}, Timestamp: {4}", CurrentData.Tag, CurrentData.XCoordinate,
+            //    CurrentData.YCoordinate, CurrentData.Altitude, CurrentData.TimeStamp);
+            HandleNewData(CurrentData);
+        }
+
+        private void HandleNewData(FormattedData currentData)
+        {
+            if (separationCalculator.IsAircraftInAirspace() == true)
+            {
+                foreach (FormattedData aircraft in AircraftsInAirspace)
+                {
+                    if (currentData.Tag == aircraft.Tag)
+                    {
+                        OldData = aircraft;
+                    }
+                }
+
+                currentData.Speed = speedCalculator.CalcuateSpeed(currentData);
+                currentData.CompassCourse = courseCalculator.CalculateCourse(currentData);
+                AircraftsInAirspace.Remove(OldData);
+                AircraftsInAirspace.Add(currentData);
+                
+            }
+            else
+                AircraftsInAirspace.Add(currentData);
         }
     }
 }
